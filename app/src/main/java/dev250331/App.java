43cacf774fae48295dc01f1043217bf5970b49a2
taeyoung0103 <parkty950103@gmail.com>
2025-03-31 18:10:39 +0900

    motivation class생성

--- app/src/main/java/dev250331/App.java
+++ app/src/main/java/dev250331/App.java
@@ -1,4 +1,7 @@
 package dev250331;
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,7 +15,14 @@ public class App {
     public void run() {
         System.out.println("== motivation 실행 ==");
 
-        int LastId = 1;
+        int lastid = 0;
+
+        //Motivation motivation0 = null;
+        //Motivation motivation1 = null;
+        //Motivation motivation2 = null;
+
+        List<Motivation> motivations = new ArrayList<Motivation>();
+
 
         while (true) {
             System.out.print("명령어)");
@@ -26,17 +36,65 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-                System.out.println("motivation : ");
-                String motivation = sc.nextLine();
+                int id = lastid + 1;
+                System.out.println("body : ");
+                String body = sc.nextLine();
                 System.out.println("source : ");
                 String source = sc.nextLine();
-                System.out.printf("%d번 motivation이 등록됨\n", LastId);
-                LastId++;
+
+                Motivation motivation = new Motivation(id, body, source);
+                //motivation.id = id;
+                //motivation.body = body;
+                //motivation.source = source;
+
+                motivations.add(motivation);
+
+                //if (motivation.id == 1) {
+                //    motivation0 = motivation;
+                //} else if  (motivation.id == 2) {
+                //    motivation1 = motivation;
+                //} else if (motivation.id == 3) {
+                //    motivation2 = motivation;
+                //}
+
+                System.out.printf("%d번 motivation이 등록됨\n", id);
+                lastid++;
             } else if (cmd.equals("list")) {
-                System.out.println("=".repeat(15));
-                System.out.printf(" 번호  /  source  /  motivation \n");
+                System.out.println("=".repeat(40));
+                System.out.printf(" id  /  source  /  body \n");
+                //System.out.println(motivation0.toString());
+                //System.out.println(motivation1.toString());
+                //System.out.println(motivation2.toString());
+                if(motivations.isEmpty()) {
+                    System.out.println("등록된 값이 없습니다.");
+                } else {
+                    System.out.println("값이 있습니다.");
+                }
+                
             }
         }
+    }
+
+}
+
+class Motivation {
+    int id;
+    String body;
+    String source;
+
+    public Motivation(int id, String body, String source) {
+        this.id = id;
+        this.body = body;
+        this.source = source;
+    }
+
+    @Override
+    public String toString() {
+        return "Motivation{" +
+            " id='" + id + "'" +
+            ", body='" + body + "'" +
+            ", source='" + source + "'" +
+            "}";
     }
 
 }
